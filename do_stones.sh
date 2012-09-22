31eec20d6caa2cb2d8aaba3234109aaeb4bfc48f
Zach Morgan <zpmorgan@gmail.com>
2012-09-22 12:47:46 -0400

    bash loop for a few more w stones.

--- do_stones.sh
+++ do_stones.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
+WSEEDS=( 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 )
 
 povray stone.pov +H300 +W300 Declare=W=0 -Ob.png +UA
-povray stone.pov +H300 +W300 Declare=W=1 -Ow1.png +UA
-povray stone.pov +H300 +W300 Declare=Seed=33 Declare=W=1 -Ow2.png +UA
-povray stone.pov +H300 +W300 Declare=Seed=83 Declare=W=1 -Ow3.png +UA
+
+for i in "${WSEEDS[@]}"
+do
+   echo $i
+   povray stone.pov +H300 +W300 Declare=Seed=${i} Declare=W=1 -Ow${i}.png +UA
+done
+
