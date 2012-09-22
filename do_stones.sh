d0c12b19accb6d8d70753aa08eb328ce61b4f1c3
Zach Morgan <zpmorgan@gmail.com>
2012-09-22 12:27:51 -0400

    transparent backgrounds, stone graphics added.

--- do_stones.sh
+++ do_stones.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-povray stone.pov +H300 +W300 Declare=W=0 -Ob.png
-povray stone.pov +H300 +W300 Declare=W=1 -Ow1.png
-povray stone.pov +H300 +W300 Declare=Seed=33 Declare=W=1 -Ow2.png
-povray stone.pov +H300 +W300 Declare=Seed=83 Declare=W=1 -Ow3.png
+povray stone.pov +H300 +W300 Declare=W=0 -Ob.png +UA
+povray stone.pov +H300 +W300 Declare=W=1 -Ow1.png +UA
+povray stone.pov +H300 +W300 Declare=Seed=33 Declare=W=1 -Ow2.png +UA
+povray stone.pov +H300 +W300 Declare=Seed=83 Declare=W=1 -Ow3.png +UA
